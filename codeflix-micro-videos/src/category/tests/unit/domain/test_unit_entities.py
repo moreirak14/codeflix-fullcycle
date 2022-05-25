1e8f53a67f09d56b0fd2d44a1c7dd825a1d28eb0
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-05-24 23:30:53 -0300

    feat: test constructor

--- codeflix-micro-videos/src/category/tests/unit/domain/test_unit_entities.py
+++ codeflix-micro-videos/src/category/tests/unit/domain/test_unit_entities.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 import unittest
 from datetime import datetime
 from category.domain.entities import Category
@@ -10,13 +11,29 @@ class TestCategoryUnit(unittest.TestCase):
         self.assertTrue(is_dataclass(Category))
 
     def test_constructor(self):
+        category = Category(name="Movie")
+        self.assertEqual(category.name, "Movie")
+        self.assertEqual(category.description, None)
+        self.assertEqual(category.is_active, True)
+        self.assertIsInstance(category.created_at, datetime)
+
+        created_at = datetime.now()
         category = Category(
             name="Movie", 
             description="some description", 
-            is_active=True,
-            created_at=datetime.now()
+            is_active=False,
+            created_at=created_at
         )
+
         self.assertEqual(category.name, "Movie")
         self.assertEqual(category.description, "some description")
-        self.assertEqual(category.is_active, True)
-        self.assertIsInstance(category.created_at, datetime)
+        self.assertEqual(category.is_active, False)
+        self.assertEqual(category.created_at, created_at)
+
+    def test_if_created_at_is_generated_in_constructor(self):
+        category1 = Category(name="Movie 1")
+        category2 = Category(name="Movie 2")
+        self.assertNotEqual(
+            category1.created_at.timestamp(),
+            category2.created_at.timestamp()
+        )
