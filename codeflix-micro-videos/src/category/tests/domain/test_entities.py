36073e1fa7f3660b0f02ae00aff70d17fc6bf979
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-05-24 22:25:32 -0300

    feat: add unitest Category

---
+++ codeflix-micro-videos/src/category/tests/domain/test_entities.py
@@ -0,0 +1,12 @@
+import unittest
+from datetime import datetime
+from category.domain.entities import Category
+
+
+class TestCategory(unittest.TestCase):
+    def test_constructor(self):
+        category = Category("Movie", "some description", True, datetime.now())
+        self.assertEqual(category.name, "Movie")
+        self.assertEqual(category.description, "some description")
+        self.assertEqual(category.is_active, True)
+        self.assertIsInstance(category.create_at, datetime)
