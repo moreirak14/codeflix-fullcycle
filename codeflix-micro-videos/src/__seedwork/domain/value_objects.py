62c1debbcaca3149bf4e25be5819dfac446060b5
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-06-03 19:32:17 -0300

    feat: criated abstract in value object

--- codeflix-micro-videos/src/__seedwork/domain/value_objects.py
+++ codeflix-micro-videos/src/__seedwork/domain/value_objects.py
@@ -1,10 +1,23 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, fields
+import json
 import uuid
 from __seedwork.domain.exceptions import InvalidUuidException
+from abc import ABC
 
 
 @dataclass(frozen=True)
-class UniqueEntityId:
+class ValueObject(ABC):
+    def __str__(self) -> str:
+        fields_name = [field.name for field in fields(self)]
+        return str(getattr(self, fields_name[0])) \
+                if len(fields_name) == 1 \
+                    else json.dumps({
+                        field_name: getattr(self, field_name) for field_name in fields_name
+                    })
+
+
+@dataclass(frozen=True)
+class UniqueEntityId(ValueObject):
 
     id: str = field(default_factory=lambda: str(uuid.uuid4()))
 
@@ -18,7 +31,3 @@ class UniqueEntityId:
             uuid.UUID(self.id)
         except ValueError as error:
             raise InvalidUuidException() from error
-
-    def __str__(self) -> str:
-        return f"{self.id}"
-
