e9edb1eb45b28b40101d0ad2072b80f9e7910a57
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-06-20 21:39:34 -0300

    feat: added validation rules in category

--- codeflix-micro-videos/src/category/domain/entities.py
+++ codeflix-micro-videos/src/category/domain/entities.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from dataclasses import dataclass, field
 from typing import Optional
 from __seedwork.domain.entities import Entity
+from __seedwork.domain.validators import ValidatorRules
 
 
 @dataclass(kw_only=True, frozen=True, slots=True)
@@ -13,7 +14,17 @@ class Category(Entity):
     created_at: Optional[datetime] = field(
         default_factory=lambda: datetime.now())
     
+    #constructor
+    def __new__(cls, **kwargs):
+        cls.validate(
+            name=kwargs.get("name"),
+            description=kwargs.get("description"),
+            is_active=kwargs.get("is_active"),
+        )
+        return super(Category, cls).__new__(cls=cls)
+
     def update(self, name: str, description: str):
+        self.validate(name, description)
         self._set('name', name)
         self._set('description', description)
 
@@ -22,3 +33,9 @@ class Category(Entity):
 
     def deactivate(self):
         self._set('is_active', False)
+    
+    @classmethod
+    def validate(cls, name: str, description: str, is_active: bool = None):
+        ValidatorRules.values(name, "name").required().string().max_length(255)
+        ValidatorRules.values(description, "description").string()
+        ValidatorRules.values(is_active, "is_active").boolean()
