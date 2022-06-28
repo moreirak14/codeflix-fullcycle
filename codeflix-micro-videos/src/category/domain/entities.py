2638405ed167213c6c693365b0933f648e6febc2
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-06-27 22:41:28 -0300

    feat: implementing category validation with DRF

--- codeflix-micro-videos/src/category/domain/entities.py
+++ codeflix-micro-videos/src/category/domain/entities.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from dataclasses import dataclass, field
 from typing import Optional
 from __seedwork.domain.entities import Entity
-from __seedwork.domain.validators import ValidatorRules
+from category.domain.validators import CategoryValidatorFactory
 
 
 @dataclass(kw_only=True, frozen=True, slots=True)
@@ -20,8 +20,13 @@ class Category(Entity):
             name=kwargs.get("name"),
             description=kwargs.get("description"),
             is_active=kwargs.get("is_active"),
+            created_at=kwargs.get("created_at"),
         )
         return super(Category, cls).__new__(cls)
+    
+    def __post_init__(self):
+        if not self.created_at:
+            self._set("created_at", datetime.now())
 
     def update(self, name: str, description: str):
         self.validate(name, description)
@@ -34,8 +39,18 @@ class Category(Entity):
     def deactivate(self):
         self._set('is_active', False)
 
+    # @classmethod
+    # def validate(cls, name: str, description: str, is_active: bool = None):
+    #     ValidatorRules.values(name, "name").required().string().max_length(255)
+    #     ValidatorRules.values(description, "description").string()
+    #     ValidatorRules.values(is_active, "is_active").boolean()
+
     @classmethod
-    def validate(cls, name: str, description: str, is_active: bool = None):
-        ValidatorRules.values(name, "name").required().string().max_length(255)
-        ValidatorRules.values(description, "description").string()
-        ValidatorRules.values(is_active, "is_active").boolean()
+    def validate(cls, name: str, description: str, is_active: bool = None, created_at: datetime = None):
+        validator = CategoryValidatorFactory.create()
+        validator.validate({
+            "name": name,
+            "description": description,
+            "is_active": is_active,
+            "created_at": created_at,
+        })
