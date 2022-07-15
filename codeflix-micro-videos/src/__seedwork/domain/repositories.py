59f2d879ca19dca271af3b8e0886657e4eb80988
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-07-14 21:14:02 -0300

    feat: criating class to organize custom search parameters

--- codeflix-micro-videos/src/__seedwork/domain/repositories.py
+++ codeflix-micro-videos/src/__seedwork/domain/repositories.py
@@ -1,7 +1,7 @@
 from abc import ABC
 import abc
 from dataclasses import dataclass, field
-from typing import Generic, List, TypeVar
+from typing import Any, Generic, List, Optional, TypeVar
 from __seedwork.domain.value_objects import UniqueEntityId
 from __seedwork.domain.entities import Entity
 from __seedwork.domain.exceptions import NotFoundException
@@ -30,6 +30,73 @@ class RepositoryInterface(Generic[ET], ABC):
     @abc.abstractmethod
     def delete(self, entity_id: str | UniqueEntityId) -> None:
         raise NotImplementedError()
+
+
+Input = TypeVar("Input")
+Output = TypeVar("Output")
+
+
+class SearchableRepositoryInterface(Generic[ET, Input, Output], RepositoryInterface[ET], ABC):
+
+    @abc.abstractmethod
+    def search(self, input_params: Input) -> Output:
+        raise NotImplementedError()
+
+
+Filter = TypeVar("Filter", str, Any)
+
+
+@dataclass(slots=True, kw_only=True)
+class SearchParams(Generic[Filter]):
+    page: Optional[int] = 1
+    per_page: Optional[int] = 15
+    sort: Optional[str] = None
+    sort_dir: Optional[str] = None
+    filter: Optional[Filter] = None
+
+    def __post_init__(self):
+        self._normalize_page()
+        self._normalize_per_page()
+        self._normalize_sort()
+        self._normalize_sort_dir()
+        self._normalize_filter()
+
+    def _normalize_page(self):
+        page = self._convert_to_int(self.page)
+        if page <= 0:
+            page = self._get_dataclass_field("page").default
+        self.page = page
+
+    def _normalize_per_page(self):
+        per_page = self._convert_to_int(self.per_page)
+        if per_page < 1:
+            per_page = self._get_dataclass_field("per_page").default
+        self.per_page = per_page
+
+    def _normalize_sort(self):
+        self.sort = None if self.sort == "" or self.sort is None else str(
+            self.sort)
+
+    def _normalize_sort_dir(self):
+        if not self.sort:
+            self.sort_dir = None
+            return
+
+        sort_dir = str(self.sort_dir).lower()
+        self.sort_dir = "asc" if sort_dir == "asc" and sort_dir == "desc" else sort_dir
+
+    def _normalize_filter(self):
+        self.filter = None if self.filter == "" or self.filter is None else str(
+            self.filter)
+
+    def _convert_to_int(self, value: Any, default=0) -> int:
+        try:
+            return int(value)
+        except (ValueError, TypeError):
+            return default
+
+    def _get_dataclass_field(self, field_name):
+        return SearchParams.__dataclass_fields__[field_name]
 
 
 @dataclass(slots=True)
