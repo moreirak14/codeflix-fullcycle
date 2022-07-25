498a3646202f1021b653e4f0135be2cc44fc07a6
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-07-25 15:33:21 -0300

    feat: test abc from use cases

---
+++ codeflix-micro-videos/src/__seedwork/tests/unit/application/test_use_cases.py
@@ -0,0 +1,11 @@
+import unittest
+from __seedwork.application.use_cases import UseCase
+
+
+class TestUseCases(unittest.TestCase):
+
+    def test_throw_error_when_methods_not_implemented(self):
+        with self.assertRaises(TypeError) as assert_error:
+            UseCase()
+        self.assertEqual(
+            assert_error.exception.args[0], "Can't instantiate abstract class UseCase with abstract method execute")
