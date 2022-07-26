633f540d7b17753765abb0bcbee0093c558b9134
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-07-26 19:35:52 -0300

    feat: creating list category use case

--- codeflix-micro-videos/src/category/application/use_cases.py
+++ codeflix-micro-videos/src/category/application/use_cases.py
@@ -1,5 +1,5 @@
 from dataclasses import asdict, dataclass
-from typing import Optional
+from typing import List, Optional
 from __seedwork.application.use_cases import UseCase
 from category.application.dto import CategoryOutput, CategoryOutputMapper
 from category.domain.entities import Category
@@ -52,3 +52,39 @@ class GetCategoryUseCase(UseCase):
     @dataclass(slots=True, frozen=True)
     class Output(CategoryOutput):
         pass
+
+
+@dataclass(slots=True, frozen=True)
+class ListCategoryUseCase(UseCase):
+
+    category_repo: CategoryRepository
+
+    def execute(self, input_param: "Input") -> "Output":
+        search_params = self.category_repo.SearchParams(**asdict(input_param))
+        result = self.category_repo.search(search_params)
+        return self.__to_output(result=result)
+
+    def __to_output(self, result: CategoryRepository.SearchResult):
+        return ListCategoryUseCase.Output(
+            items=list(map(CategoryOutputMapper.to_output, result.items)),
+            total=result.total,
+            current_page=result.current_page,
+            per_page=result.per_page,
+            last_page=result.last_page,
+        )
+
+    @dataclass(slots=True, frozen=True)
+    class Input:
+        page: Optional[int] = None
+        per_page: Optional[int] = None
+        sort: Optional[str] = None
+        sort_dir: Optional[str] = None
+        filter: Optional[str] = None
+
+    @dataclass(slots=True, frozen=True)
+    class Output:
+        items: List[CategoryOutput]
+        total: int
+        current_page: int
+        per_page: int
+        last_page: int
