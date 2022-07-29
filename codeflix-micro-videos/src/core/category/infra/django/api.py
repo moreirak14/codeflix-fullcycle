f8722a723061da6bccc804c65975fa2d6f9c4066
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-07-29 19:13:21 -0300

    feat: created get_object with id as parameter

--- codeflix-micro-videos/src/core/category/infra/django/api.py
+++ codeflix-micro-videos/src/core/category/infra/django/api.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from core.category.application.use_cases import (
     CreateCategoryUseCase,
+    GetCategoryUseCase,
     ListCategoryUseCase,
 )
 
@@ -14,13 +15,21 @@ class CategoryResource(APIView):
 
     create_use_case: Callable[[], CreateCategoryUseCase]
     list_use_case: Callable[[], ListCategoryUseCase]
+    get_use_case: Callable[[], GetCategoryUseCase]
 
     def post(self, request: Request):
         input_param = CreateCategoryUseCase.Input(**request.data)
         output = self.create_use_case().execute(input_param=input_param)
         return Response(asdict(output), status=status.HTTP_201_CREATED)
 
-    def get(self, request: Request):
+    def get(self, request: Request, id: str = None):  # pylint: disable=redefined-builtin invalid-name
+        if id:
+            return self.get_object(id=id)
         input_param = ListCategoryUseCase.Input(**request.query_params.dict())
         output = self.list_use_case().execute(input_param=input_param)
+        return Response(asdict(output), status=status.HTTP_200_OK)
+
+    def get_object(self, id: str):  # pylint: disable=redefined-builtin invalid-name
+        input_param = GetCategoryUseCase.Input(id=id)
+        output = self.get_use_case().execute(input_param=input_param)
         return Response(asdict(output), status=status.HTTP_200_OK)
