cda382c9d8bd2af73675e3495191560a6b2d27bc
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-07-29 20:16:43 -0300

    feat: added delete category dependency_injector

--- codeflix-micro-videos/src/core/category/infra/django/urls.py
+++ codeflix-micro-videos/src/core/category/infra/django/urls.py
@@ -9,14 +9,13 @@ def __init_category_resource():
         "list_use_case": container.use_case_category_list_category,
         "get_use_case": container.use_case_category_get_category,
         "update_use_case": container.use_case_category_update_category,
+        "delete_use_case": container.use_case_category_delete_category,
     }
 
 
 urlpatterns = [
-    path("categories/", CategoryResource.as_view(
-        **__init_category_resource()
-    )),
-    path("categories/<uuid:id>/", CategoryResource.as_view(
-        **__init_category_resource()
-    )),
+    path("categories/", CategoryResource.as_view(**__init_category_resource())),
+    path(
+        "categories/<uuid:id>/", CategoryResource.as_view(**__init_category_resource())
+    ),
 ]
--- codeflix-micro-videos/src/django_app/container.py
+++ codeflix-micro-videos/src/django_app/container.py
@@ -1,21 +1,34 @@
 from dependency_injector import containers, providers
 from core.category.infra.in_memory.repositories import CategoryInMemoryRepository
-from core.category.application.use_cases import CreateCategoryUseCase, GetCategoryUseCase, ListCategoryUseCase, UpdateCategoryUseCase
+from core.category.application.use_cases import (
+    CreateCategoryUseCase,
+    DeleteCategoryUseCase,
+    GetCategoryUseCase,
+    ListCategoryUseCase,
+    UpdateCategoryUseCase,
+)
 
 
 class Container(containers.DeclarativeContainer):
 
-    repository_category_in_memory = providers.Singleton(
-        CategoryInMemoryRepository)
+    repository_category_in_memory = providers.Singleton(CategoryInMemoryRepository)
 
     use_case_category_create_category = providers.Singleton(
-        CreateCategoryUseCase, category_repo=repository_category_in_memory)
+        CreateCategoryUseCase, category_repo=repository_category_in_memory
+    )
 
     use_case_category_list_category = providers.Singleton(
-        ListCategoryUseCase, category_repo=repository_category_in_memory)
+        ListCategoryUseCase, category_repo=repository_category_in_memory
+    )
 
     use_case_category_get_category = providers.Singleton(
-        GetCategoryUseCase, category_repo=repository_category_in_memory)
+        GetCategoryUseCase, category_repo=repository_category_in_memory
+    )
 
     use_case_category_update_category = providers.Singleton(
-        UpdateCategoryUseCase, category_repo=repository_category_in_memory)
+        UpdateCategoryUseCase, category_repo=repository_category_in_memory
+    )
+
+    use_case_category_delete_category = providers.Singleton(
+        DeleteCategoryUseCase, category_repo=repository_category_in_memory
+    )
