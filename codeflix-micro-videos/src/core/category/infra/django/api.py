57eead1d9a49acb82338e3c970bb3172bcc1c850
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-07-29 20:17:11 -0300

    feat: craete delete category_resource

--- codeflix-micro-videos/src/core/category/infra/django/api.py
+++ codeflix-micro-videos/src/core/category/infra/django/api.py
@@ -6,10 +6,12 @@ from rest_framework.views import APIView
 from rest_framework import status
 from core.category.application.use_cases import (
     CreateCategoryUseCase,
+    DeleteCategoryUseCase,
     GetCategoryUseCase,
     ListCategoryUseCase,
     UpdateCategoryUseCase,
 )
+
 
 @dataclass(slots=True)
 class CategoryResource(APIView):
@@ -18,13 +20,16 @@ class CategoryResource(APIView):
     list_use_case: Callable[[], ListCategoryUseCase]
     get_use_case: Callable[[], GetCategoryUseCase]
     update_use_case: Callable[[], UpdateCategoryUseCase]
+    delete_use_case: Callable[[], DeleteCategoryUseCase]
 
     def post(self, request: Request):
         input_param = CreateCategoryUseCase.Input(**request.data)
         output = self.create_use_case().execute(input_param=input_param)
         return Response(asdict(output), status=status.HTTP_201_CREATED)
 
-    def get(self, request: Request, id: str = None):  # pylint: disable=redefined-builtin invalid-name
+    def get(
+        self, request: Request, id: str = None
+    ):  # pylint: disable=redefined-builtin invalid-name
         if id:
             return self.get_object(id=id)
         input_param = ListCategoryUseCase.Input(**request.query_params.dict())
@@ -36,7 +41,16 @@ class CategoryResource(APIView):
         output = self.get_use_case().execute(input_param=input_param)
         return Response(asdict(output), status=status.HTTP_200_OK)
 
-    def put(self, request: Request, id: str):  # pylint: disable=redefined-builtin invalid-name
+    def put(
+        self, request: Request, id: str
+    ):  # pylint: disable=redefined-builtin invalid-name
         input_param = UpdateCategoryUseCase.Input(**{"id": id, **request.data})
         output = self.update_use_case().execute(input_param=input_param)
         return Response(asdict(output), status=status.HTTP_200_OK)
+
+    def delete(
+        self, _request: Request, id: str
+    ):  # pylint: disable=redefined-builtin invalid-name
+        input_param = DeleteCategoryUseCase.Input(id=id)
+        self.delete_use_case().execute(input_param=input_param)
+        return Response(status=status.HTTP_204_NO_CONTENT)
