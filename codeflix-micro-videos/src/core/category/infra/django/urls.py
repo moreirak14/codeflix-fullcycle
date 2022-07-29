970986d8671622743499b1179373cbdc5c8e1af5
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-07-29 19:12:14 -0300

    feat: added list and get category dependency injectior

--- codeflix-micro-videos/src/core/category/infra/django/urls.py
+++ codeflix-micro-videos/src/core/category/infra/django/urls.py
@@ -1,30 +1,21 @@
 from django.urls import path
 from django_app import container
-from core.category.application.use_cases import CreateCategoryUseCase
-from core.category.infra.in_memory.repositories import CategoryInMemoryRepository
 from .api import CategoryResource
 
 
-class CategoryInMemoryRepositoryFactory:
-
-    repo: CategoryInMemoryRepository = None
-
-    @classmethod
-    def create(cls):
-        if not cls.repo:
-            cls.repo = CategoryInMemoryRepository()
-        return cls.repo
-
-
-class CreateCategoryUseCaseFactory:
-
-    @staticmethod
-    def create():
-        repo = CategoryInMemoryRepositoryFactory.create()
-        return CreateCategoryUseCase(repo)
+def __init_category_resource():
+    return {
+        "create_use_case": container.use_case_category_create_category,
+        "list_use_case": container.use_case_category_list_category,
+        "get_use_case": container.use_case_category_get_category,
+    }
 
 
 urlpatterns = [
     path("categories/", CategoryResource.as_view(
-        create_use_case=container.use_case_category_create_category
-    ))]
+        **__init_category_resource()
+    )),
+    path("categories/<uuid:id>/", CategoryResource.as_view(
+        **__init_category_resource()
+    )),
+]
--- codeflix-micro-videos/src/django_app/container.py
+++ codeflix-micro-videos/src/django_app/container.py
@@ -1,6 +1,6 @@
 from dependency_injector import containers, providers
 from core.category.infra.in_memory.repositories import CategoryInMemoryRepository
-from core.category.application.use_cases import CreateCategoryUseCase
+from core.category.application.use_cases import CreateCategoryUseCase, GetCategoryUseCase, ListCategoryUseCase
 
 
 class Container(containers.DeclarativeContainer):
@@ -10,3 +10,9 @@ class Container(containers.DeclarativeContainer):
 
     use_case_category_create_category = providers.Singleton(
         CreateCategoryUseCase, category_repo=repository_category_in_memory)
+
+    use_case_category_list_category = providers.Singleton(
+        ListCategoryUseCase, category_repo=repository_category_in_memory)
+
+    use_case_category_get_category = providers.Singleton(
+        GetCategoryUseCase, category_repo=repository_category_in_memory)
