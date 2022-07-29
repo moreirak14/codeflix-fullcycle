ed343766f85cdb08fc1caa011e767e21b8c25fef
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-07-29 19:14:01 -0300

    feat: test get_object with id as parameter

--- codeflix-micro-videos/src/core/category/tests/unit/infra/django/test_unit_api.py
+++ codeflix-micro-videos/src/core/category/tests/unit/infra/django/test_unit_api.py
@@ -5,7 +5,7 @@ from core.category.application.dto import CategoryOutput
 from rest_framework import status
 from rest_framework.test import APIRequestFactory
 from rest_framework.request import Request
-from core.category.application.use_cases import CreateCategoryUseCase, ListCategoryUseCase
+from core.category.application.use_cases import CreateCategoryUseCase, GetCategoryUseCase, ListCategoryUseCase
 from core.category.infra.django.api import CategoryResource
 
 
@@ -14,7 +14,8 @@ class TestCategoryResourceUnit(unittest.TestCase):
     def __init_all_none(self):
         return {
             "list_use_case": None,
-            "create_use_case": None
+            "create_use_case": None,
+            "get_use_case": None,
         }
 
     def test_post_method(self):
@@ -46,7 +47,7 @@ class TestCategoryResourceUnit(unittest.TestCase):
             "created_at": mock_create_use_case.execute.return_value.created_at,
         })
 
-    def test_list_method(self):
+    def test_get_method(self):
         mock_list_use_case = mock.Mock(CreateCategoryUseCase)
         mock_list_use_case.execute.return_value = ListCategoryUseCase.Output(
             items=[
@@ -96,3 +97,62 @@ class TestCategoryResourceUnit(unittest.TestCase):
                 "per_page": 2,
                 "last_page": 1
             },)
+
+    def test_if_get_invoke_get_object(self):
+        mock_list_use_case = mock.Mock(ListCategoryUseCase)
+        mock_get_use_case = mock.Mock(GetCategoryUseCase)
+        mock_get_use_case.execute.return_value = GetCategoryUseCase.Output(
+            id="6eac08e5-5a54-4d2b-afeb-16253d0e75fb",
+            name="Movie",
+            description=None,
+            is_active=True,
+            created_at=datetime.now(),
+        )
+
+        resource = CategoryResource(
+            **{**self.__init_all_none(),
+               "list_use_case": lambda: mock_list_use_case,
+               "get_use_case": lambda: mock_get_use_case,
+               })
+        response = resource.get(
+            None,
+            id="6eac08e5-5a54-4d2b-afeb-16253d0e75fb"
+        )
+
+        mock_get_use_case.execute.assert_called_with(
+            input_param=GetCategoryUseCase.Input(id="6eac08e5-5a54-4d2b-afeb-16253d0e75fb"))
+        self.assertEqual(mock_list_use_case.call_count, 0)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {
+            "id": "6eac08e5-5a54-4d2b-afeb-16253d0e75fb",
+            "name": "Movie",
+            "description": None,
+            "is_active": True,
+            "created_at": mock_get_use_case.execute.return_value.created_at,
+        })
+
+    def test_get_object_method(self):
+        mock_get_use_case = mock.Mock(GetCategoryUseCase)
+        mock_get_use_case.execute.return_value = GetCategoryUseCase.Output(
+            id="6eac08e5-5a54-4d2b-afeb-16253d0e75fb",
+            name="Movie",
+            description=None,
+            is_active=True,
+            created_at=datetime.now(),
+        )
+
+        resource = CategoryResource(
+            **{**self.__init_all_none(), "get_use_case": lambda: mock_get_use_case})
+        response = resource.get_object(
+            id="6eac08e5-5a54-4d2b-afeb-16253d0e75fb")
+
+        mock_get_use_case.execute.assert_called_with(
+            input_param=GetCategoryUseCase.Input(id="6eac08e5-5a54-4d2b-afeb-16253d0e75fb"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {
+            "id": "6eac08e5-5a54-4d2b-afeb-16253d0e75fb",
+            "name": "Movie",
+            "description": None,
+            "is_active": True,
+            "created_at": mock_get_use_case.execute.return_value.created_at,
+        })
