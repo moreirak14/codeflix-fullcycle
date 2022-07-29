d26e5b38a3f22fafaad3d3972219f5d88dfb8bf8
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-07-29 15:24:04 -0300

    feat: refactor after of container

--- codeflix-micro-videos/src/core/category/infra/django/api.py
+++ codeflix-micro-videos/src/core/category/infra/django/api.py
@@ -1,4 +1,5 @@
 from dataclasses import asdict
+from typing import Callable
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
@@ -6,29 +7,19 @@ from core.category.application.use_cases import (
     CreateCategoryUseCase,
     ListCategoryUseCase,
 )
-from core.category.infra.in_memory.repositories import CategoryInMemoryRepository
 
 
 class CategoryResource(APIView):
 
-    repo = CategoryInMemoryRepository()
+    create_use_case: Callable[[], CreateCategoryUseCase]
+    list_use_case: Callable[[], ListCategoryUseCase]
 
     def post(self, request: Request):
-        create_use_case = CreateCategoryUseCase(self.repo)
         input_param = CreateCategoryUseCase.Input(name=request.data["name"])
-        output = create_use_case.execute(input_param=input_param)
+        output = self.create_use_case().execute(input_param=input_param)
         return Response(asdict(output))
 
     def get(self, request: Request):
-        list_use_case = ListCategoryUseCase(self.repo)
-        input_param = ListCategoryUseCase.Input()
-        output = list_use_case.execute(input_param=input_param)
+        input_param = ListCategoryUseCase.Input(**request.query_params.dict())
+        output = self.list_use_case().execute(input_param=input_param)
         return Response(asdict(output))
-
-
-# @api_view(["POST"])
-# def hello_world(request: Request):
-#     create_use_case = CreateCategoryUseCase(CategoryInMemoryRepository())
-#     input_param = CreateCategoryUseCase.Input(name=request.data["name"])
-#     output = create_use_case.execute(input_param=input_param)
-#     return Response(asdict(output))
--- codeflix-micro-videos/src/core/category/infra/django/urls.py
+++ codeflix-micro-videos/src/core/category/infra/django/urls.py
@@ -1,5 +1,27 @@
 from django.urls import path
+from django_app import container
 from .api import CategoryResource
+from core.category.application.use_cases import CreateCategoryUseCase
+from core.category.infra.in_memory.repositories import CategoryInMemoryRepository
 
 
-urlpatterns = [path("categories/", CategoryResource.as_view())]
+class CategoryInMemoryRepositoryFactory:
+
+    repo: CategoryInMemoryRepository = None
+
+    @staticmethod
+    def create(cls):
+        if not cls.repo:
+            cls.repo = CategoryInMemoryRepository()
+        return cls.repo
+
+class CreateCategoryUseCaseFactory:
+
+    @staticmethod
+    def create():
+        repo = CategoryInMemoryRepositoryFactory.create()
+        return CreateCategoryUseCase(repo)
+
+
+urlpatterns = [
+    path("categories/", CategoryResource.as_view(create_use_case = container.use_case_category_create_category))]
