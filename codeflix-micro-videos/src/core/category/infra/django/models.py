bf87c27fdf3f26ee1816677067285afbbabc79a7
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-07-30 17:07:32 -0300

    feat: create CategoryModel

--- codeflix-micro-videos/src/core/category/infra/django/models.py
+++ codeflix-micro-videos/src/core/category/infra/django/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
-# Create your models here.
-
 
 class CategoryModel(models.Model):
+    id = models.UUIDField(primary_key=True, editable=True)
     name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField()
+
+    class Meta:
+        db_table = "categories"
